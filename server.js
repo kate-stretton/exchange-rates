7ad5b67f56a19b08e6b2c1defb9539ff59c04583
= <=>
2025-04-13 10:27:05 +1200

    return an average of the results for both apis

--- server.js
+++ server.js
@@ -6,21 +6,55 @@ const port = 3000
 // Middleware
 app.use(express.json())
 
-// Simple fetch for frankfurter api using NZD as base currency
+// Fetch and average rates from both apis
 app.get('/exchange-rates', (req, res) => {
-  axios
-    .get('https://api.frankfurter.dev/v1/latest?base=NZD')
-    .then((response) => res.json(response.data))
-    .catch((error) => res.status(500).json({ error }))
-})
+  // Helper function to calculate average rates
+  const calculateAverageRates = (frankfurterRates, exchangeApiRates) => {
+    const average = {}
 
-// Additional fetch for exchange-api using NZD as base currency
-app.get('/exchange-rates/2', (req, res) => {
-  axios
-    .get(
-      'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/nzd.json'
-    )
-    .then((response) => res.json(response.data))
+    const allCurrencies = new Set([
+      ...Object.keys(frankfurterRates),
+      ...Object.keys(exchangeApiRates),
+    ])
+
+    allCurrencies.forEach((currency) => {
+      const frankfurterRate = frankfurterRates[currency]
+      const exchangeRate = exchangeApiRates[currency]
+
+      if (frankfurterRate && exchangeRate) {
+        average[currency] = (frankfurterRate + exchangeRate) / 2
+      } else {
+        average[currency] = frankfurterRate || exchangeRate
+      }
+    })
+
+    return average
+  }
+
+  // API requests
+  const frankfurterRequest = axios.get(
+    'https://api.frankfurter.dev/v1/latest?base=NZD'
+  )
+  const exchangeApiRequest = axios.get(
+    'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/nzd.json'
+  )
+
+  Promise.all([frankfurterRequest, exchangeApiRequest])
+    .then(([frankfurterResponse, exchangeApiResponse]) => {
+      const frankfurterRates = frankfurterResponse.data.rates
+      const exchangeApiRates = exchangeApiResponse.data.nzd
+
+      const averageRates = calculateAverageRates(
+        frankfurterRates,
+        exchangeApiRates
+      )
+
+      res.json({
+        datasource: 'Free Currency Rates API',
+        base: 'NZD',
+        rates: averageRates,
+      })
+    })
     .catch((error) => res.status(500).json({ error }))
 })
 
